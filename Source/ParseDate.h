9b19fc7eed49b5bf666547deb128b127efdc0433
Nicholas Riley <pester@sabi.net>
2007-12-02 03:51:55 +0000

    DynaLoader-10.[45].a: Tiger and Leopard versions, since Tiger's DynaLoader.a crashes on Leopard ppc.
    DynaLoader.a: Renamed to DynaLoader-10.4.a.
    
    English.lproj/InfoPlist.strings: Updated for 1.1b7.
    
    Info-Pester.plist: Updated for 1.1b7.
    
    NJRDateFormatter.m: Conditionally load ParseDate dylib (see above).
    
    ParseDate.[hm]: Split out from NJRDateFormatter.m.
    
    Read Me.rtfd/TXT.rtf: Updated for 1.1b7.
    
    
    
    git-svn-id: http://dev.sabi.net/svn/dev/trunk/Cocoa/Pester@367 24bc951d-4bb6-0310-a7b7-f2527510289e

---
+++ Source/ParseDate.h
@@ -0,0 +1,11 @@
+//
+//  ParseDate.h
+//  Pester
+//
+//  Created by Nicholas Riley on 11/28/07.
+//  Copyright 2007 Nicholas Riley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSDate *(*parse_natural_language_date)(NSString *) = NULL;
