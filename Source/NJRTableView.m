39ed0c7cb4b76333e61440c6489dd14af6802acc
Nicholas Riley <pester@sabi.net>
2009-12-19 23:50:16 -0600

    Remove selectString:inTableView: which seems unused.

--- Source/NJRTableView.m
+++ Source/NJRTableView.m
@@ -62,31 +62,29 @@
 
 - (void)insertText:(id)inString;
 {
-    if (![[self delegate] respondsToSelector:@selector(selectString:inTableView:)]) {
-        // For consistency with List Manager as documented, reset the typeahead buffer after twice the delay until key repeat (in ticks).
-        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-        int keyRepeatTicks = [defaults integerForKey: @"InitialKeyRepeat"];
-        NSTimeInterval resetDelay;
+    // For consistency with List Manager as documented, reset the typeahead buffer after twice the delay until key repeat (in ticks).
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    int keyRepeatTicks = [defaults integerForKey: @"InitialKeyRepeat"];
+    NSTimeInterval resetDelay;
 
-        if (keyRepeatTicks == 0) keyRepeatTicks = 35; // default may be missing; if so, set default
+    if (keyRepeatTicks == 0) keyRepeatTicks = 35; // default may be missing; if so, set default
 
-        resetDelay = MIN(2.0 / 60.0 * keyRepeatTicks, 2.0);
+    resetDelay = MIN(2.0 / 60.0 * keyRepeatTicks, 2.0);
 
-        if (typed == nil) typed = [[NSMutableString alloc] init];
-        [typed appendString: inString];
+    if (typed == nil) typed = [[NSMutableString alloc] init];
+    [typed appendString: inString];
 
-        // Cancel any previously queued future invocations of _resetTypeSelect
-        [NSObject cancelPreviousPerformRequestsWithTarget: self selector: @selector(_resetTypeSelect) object: nil];
+    // Cancel any previously queued future invocations of _resetTypeSelect
+    [NSObject cancelPreviousPerformRequestsWithTarget: self selector: @selector(_resetTypeSelect) object: nil];
 
-        // queue an invocation of clearAccumulatingTypeahead for the near future.
-        [self performSelector: @selector(_resetTypeSelect) withObject: nil afterDelay: resetDelay];
+    // queue an invocation of clearAccumulatingTypeahead for the near future.
+    [self performSelector: @selector(_resetTypeSelect) withObject: nil afterDelay: resetDelay];
 
-        // Use stringWithString to make an autoreleased copy, since we may clear out the original string below before it can be used.
-        [[self delegate] tableView: self selectRowMatchingString: [NSString stringWithString: typed]];
+    // Use stringWithString to make an autoreleased copy, since we may clear out the original string below before it can be used.
+    [[self delegate] tableView: self selectRowMatchingString: [NSString stringWithString: typed]];
 
-        // Show the current typeahead string in the optional display field, like CodeWarrior does (well, not really, CW is much more elegant because it doesn't select anything until you stop typing)
-        [typeSelectDisplay setObjectValue: typed];
-    }
+    // Show the current typeahead string in the optional display field, like CodeWarrior does (well, not really, CW is much more elegant because it doesn't select anything until you stop typing)
+    [typeSelectDisplay setObjectValue: typed];
 }
 
 - (void)_resetTypeSelect;
