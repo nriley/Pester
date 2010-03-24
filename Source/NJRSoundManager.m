b7acb2efdf03346d83dde964d070eac28e067948
Nicholas Riley <pester@sabi.net>
2010-03-24 14:34:41 -0500

    Now we cache the default sound device in NJRSoundDevice, don't do it in NJRSoundManager too.

--- Source/NJRSoundManager.m
+++ Source/NJRSoundManager.m
@@ -8,53 +8,41 @@
 
 #import "NJRSoundManager.h"
 #import "NJRSoundDevice.h"
-#import <CoreAudio/CoreAudio.h>
 
 @implementation NJRSoundManager
 
-NJRSoundDevice *defaultOutputDevice;
 
 + (BOOL)volumeIsNotMutedOrInvalid:(float)volume;
 {
     return (volume > 0 && volume <= 1);
 }
 
-+ (BOOL)_getDefaultOutputDevice;
 {
-    defaultOutputDevice = [NJRSoundDevice defaultOutputDevice];
-    
-    return (defaultOutputDevice != nil);
 }
 
 + (BOOL)getDefaultOutputVolume:(float *)volume;
 {
-    if (![self _getDefaultOutputDevice]) return NO;
-    
-    return [defaultOutputDevice getOutputVolume: volume];
+    return [[NJRSoundDevice defaultOutputDevice] getOutputVolume: volume];
 }
 
 + (BOOL)saveDefaultOutputVolume;
 {
-    if (![self getDefaultOutputVolume: NULL]) return NO;
-    
-    return [defaultOutputDevice saveOutputVolume];
+    return [[NJRSoundDevice defaultOutputDevice] saveOutputVolume];
 }
 
 + (void)setDefaultOutputVolume:(float)volume;
 {
-    if (![self _getDefaultOutputDevice]) return;
-
-    [defaultOutputDevice setOutputVolume: volume];
+    [[NJRSoundDevice defaultOutputDevice] setOutputVolume: volume];
 }
 
 + (void)restoreSavedDefaultOutputVolume;
 {
-    [defaultOutputDevice restoreSavedOutputVolume];
+    [[NJRSoundDevice defaultOutputDevice] restoreSavedOutputVolume];
 }
 
 + (void)restoreSavedDefaultOutputVolumeIfCurrently:(float)volume;
 {
-    [defaultOutputDevice restoreSavedOutputVolumeIfCurrently: volume];
+    [[NJRSoundDevice defaultOutputDevice] restoreSavedOutputVolumeIfCurrently: volume];
 }
 
 @end
