68be21977a013ebabe199250992a9e596c22bd48
Nicholas Riley <pester@sabi.net>
2013-06-29 00:53:47 -0500

    Restore compatibility with Clang from Xcode 3.2.6.

--- Source/Pester_Prefix.h
+++ Source/Pester_Prefix.h
@@ -4,4 +4,8 @@
 
 #ifdef __OBJC__
     #import <Cocoa/Cocoa.h>
+    #if !__has_feature(objc_instancetype)
+	// Compile with "LLVM compiler 1.7" in Xcode 3.2.6
+	#define instancetype id
+    #endif
 #endif
