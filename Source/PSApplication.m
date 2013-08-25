75dd72a1969eb7929ad7d0c30fda8b38c58c2577
Nicholas Riley <com-github@sabi.net>
2013-08-24 23:54:14 -0500

    Work around OS X bug in which the Credits text is not centered.
    <http://www.openradar.me/14829080>

--- Source/PSApplication.m
+++ Source/PSApplication.m
@@ -126,6 +126,39 @@ static NSString * const PSShowDockCountdown = @"PesterShowDockCountdown"; // NSU
     [preferencesController showWindow: self];
 }
 
+- (void)orderFrontStandardAboutPanelWithOptions:(NSDictionary *)optionsDictionary;
+{
+    // XXX work around bug in OS X 10.7 / 10.8 where the Credits text is not centered (r. 14829080)
+    NSSet *windowsBefore = [NSSet setWithArray:[NSApp windows]];
+
+    [super orderFrontStandardAboutPanelWithOptions:optionsDictionary];
+
+    for (NSWindow *window in [NSApp windows]) {
+        if ([windowsBefore containsObject:window])
+            continue;
+
+        for (NSView *view in [[window contentView] subviews]) {
+            if (![view isKindOfClass:[NSScrollView class]])
+                continue;
+
+            NSClipView *clipView = [(NSScrollView *)view contentView];
+            NSRect clipViewFrame = [clipView frame];
+            NSView *documentView = [clipView documentView];
+            NSRect documentViewFrame = [documentView frame];
+
+            if (clipViewFrame.size.height != documentViewFrame.size.height)
+                continue; // don't mess with a scrollable view
+
+            if (clipViewFrame.size.width != documentViewFrame.size.width) {
+                documentViewFrame.size.width = clipViewFrame.size.width;
+                [documentView setFrame:documentViewFrame];
+                break;
+            }
+        }
+        break;
+    }
+}
+
 #pragma mark Spaces interaction
 
 - (void)orderOutSetAlarmPanelIfHidden;
