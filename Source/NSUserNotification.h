2c8695259256db4d0a032fda8cfcdbd2275ecbe4
Nicholas Riley <com-github@sabi.net>
2014-11-08 15:58:15 -0500

    Remove public NSUserNotification stuff now the 10.9 SDK is required.

--- Source/NSUserNotification.h
+++ Source/NSUserNotification.h
@@ -1,20 +1,3 @@
-// 10.8+
-
-@class NSString, NSImage, NSAttributedString;
-@protocol NSUserNotificationCenterDelegate;
-
-/* typedef */ enum {
-    NSUserNotificationActivationTypeReplied = 3 // 10.9
-}; /* NSUserNotificationActivationType */
-
-@interface NSObject (NSUserNotification)
-@property (copy) NSString *identifier; // 10.9
-@property (copy) NSImage *contentImage; // 10.9
-@property BOOL hasReplyButton; // 10.9
-@property (copy) NSString *responsePlaceholder; // 10.9
-@property (readonly) NSAttributedString *response; // 10.9
-@end
-
 @interface NSObject (NSUserNotificationCenter)
 - (void)_removeAllPresentedAlerts; // 10.8; subsumed by removeAllDeliveredNotifications on 10.9
 @end
