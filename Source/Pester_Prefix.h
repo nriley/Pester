b70923c9dc77371c24b3d2183b0573969602eca0
Nicholas Riley <com-github@sabi.net>
2014-11-09 21:53:03 -0500

    Fix Pester prefix header to import Cocoa again.

--- Source/Pester_Prefix.h
+++ Source/Pester_Prefix.h
@@ -2,4 +2,7 @@
 // Prefix header for all source files of the 'Pester' target in the 'Pester' project
 //
 
-#import "NJROperatingSystemVersion.h"
+#ifdef __OBJC__
+    #import <Cocoa/Cocoa.h>
+    #import "NJROperatingSystemVersion.h"
+#endif
