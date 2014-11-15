ec3120b9b4449353f7c40558a83a3fd2715f34db
Nicholas Riley <com-github@sabi.net>
2014-11-15 18:27:40 -0500

    Remove pre-10.6 volume window display code.

--- Source/PSVolumeController.m
+++ Source/PSVolumeController.m
@@ -9,17 +9,6 @@
 #import "PSVolumeController.h"
 #import "NJRNonCenteringWindow.h"
 #import "NJRSoundDevice.h"
-#include <Carbon/Carbon.h>
-
-@interface NSMenu (SnowLeopardAdditions)
-- (BOOL)popUpMenuPositioningItem:(NSMenuItem *)item atLocation:(NSPoint)location inView:(NSView *)view;
-- (void)setAllowsContextMenuPlugIns:(BOOL)allows;
-- (void)cancelTracking;
-@end
-
-@interface NSMenuItem (SnowLeopardAdditions)
-- (void)setView:(NSView *)view;
-@end
 
 @implementation PSVolumeController
 
@@ -40,52 +29,22 @@
 
 	NSView *view = [aDelegate volumeControllerLaunchingView: self];
 
-	// In 10.6, we can no longer force the modal session to work by "seeding" the slider with a mouse-down event.
-	// Instead, use a menu.  (This should mostly work on 10.5 too, but is currently untested.)
-	if ([NSMenu instancesRespondToSelector: @selector(popUpMenuPositioningItem:atLocation:inView:)]) {
-	    menu = [[NSMenu alloc] initWithTitle: @""];
-	    NSMenuItem *menuItem = [[NSMenuItem alloc] init];
-	    [menuItem setView: contentView];
-	    [menu addItem: menuItem];
-	    [menuItem release];
-	    NSPoint point;
-	    if (view != nil) {
-		NSSize size = [view bounds].size;
-		point = [view isFlipped] ? NSMakePoint(0, size.height) : NSZeroPoint;
-	    } else {
-		point = [NSEvent mouseLocation];
-	    }
-	    [menu setAllowsContextMenuPlugIns: NO];
-	    // replace with http://waffle.wootest.net/2007/08/07/popping-up-a-menu-in-cocoa/
-	    [menu popUpMenuPositioningItem: nil atLocation: point inView: view];
-	    [menu release];
-	} else {
-	    NSWindow *window = [[NJRNonCenteringWindow alloc] initWithContentRect: [contentView bounds] styleMask: NSBorderlessWindowMask backing: NSBackingStoreBuffered defer: NO];
-	    [window setContentView: contentView];
-	    [window setOpaque: NO];
-	    [window setBackgroundColor: [NSColor colorWithCalibratedWhite: 0.81f alpha: 0.9f]];
-	    [window setHasShadow: YES];
-	    [window setOneShot: YES];
-	    [window setDelegate: self];
-
-	    if (view != nil) {
-		NSWindow *parentWindow = [view window];
-		NSRect rect = [parentWindow convertRectToScreen: [view convertRect: [view bounds] toView: nil]];
-		rect.origin.x -= [window frame].size.width - rect.size.width + 1;
-		[window setFrameTopLeftPoint: rect.origin];
-		NSRect visibleFrame = [[parentWindow screen] visibleFrame];
-		if (!NSContainsRect(visibleFrame, [window frame])) {
-		    NSPoint viewTopLeft = { rect.origin.x, rect.origin.y + rect.size.height };
-		    [window setFrameOrigin: viewTopLeft];
-		}
-	    }
-	    // -[NSApplication beginModalSessionForWindow:] shows and centers the window; we use NJRNonCenteringWindow to prevent the repositioning from succeeding
-	    NSModalSession session = [NSApp beginModalSessionForWindow: window];
-	    [volumeSlider mouseDown: [NSApp currentEvent]];
-	    [NSApp runModalSession: session];
-	    [NSApp endModalSession: session];
-	    [window close];
-	}
+        menu = [[NSMenu alloc] initWithTitle: @""];
+        NSMenuItem *menuItem = [[NSMenuItem alloc] init];
+        [menuItem setView: contentView];
+        [menu addItem: menuItem];
+        [menuItem release];
+        NSPoint point;
+        if (view != nil) {
+            NSSize size = [view bounds].size;
+            point = [view isFlipped] ? NSMakePoint(0, size.height) : NSZeroPoint;
+        } else {
+            point = [NSEvent mouseLocation];
+        }
+        [menu setAllowsContextMenuPlugIns: NO];
+        // replace with http://waffle.wootest.net/2007/08/07/popping-up-a-menu-in-cocoa/
+        [menu popUpMenuPositioningItem: nil atLocation: point inView: view];
+        [menu release];
 
         [self autorelease];
     }
