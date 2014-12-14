6dc3c8a27e5344eb6f699b43da985bea94e6d74f
Nicholas Riley <com-github@sabi.net>
2014-12-14 14:38:22 -0500

    Test for Date::Manip support for date completions in plists.
    Establishes we have the same failures as in the past (see comments in ParseDate.m), except for Croatian which I had forgotten was present at all.

--- Source/ParseDateTest/ParseDateTest.m
+++ Source/ParseDateTest/ParseDateTest.m
@@ -69,14 +69,23 @@
     XCTAssertNil(parse_natural_language_date_on_queue(nil));
 }
 
+- (BOOL)setUpForLocaleLanguageCode:(NSString *)localeLanguageCode;
+{
+    init_date_parser_on_queue(localeLanguageCode);
+    if (parse_natural_language_date_on_queue(nil) != nil) {
+        XCTFail(@"initializing locale language code '%@'", localeLanguageCode);
+        return NO;
+    }
+
+    calendar.locale = [NSLocale localeWithLocaleIdentifier:localeLanguageCode];
+    return YES;
+}
+
 - (void)testWeekdaySupportedLocales;
 {
     for (NSString *localeLanguageCode in date_parser_supported_locale_language_codes()) {
-        init_date_parser_on_queue(localeLanguageCode);
-        XCTAssertNil(parse_natural_language_date_on_queue(nil), @"failed to initialize locale language code '%@'", localeLanguageCode);
-
-        // weekdays
-        calendar.locale = [NSLocale localeWithLocaleIdentifier:localeLanguageCode];
+        if (![self setUpForLocaleLanguageCode:localeLanguageCode])
+            continue;
 
         NSInteger weekday = 1;
         for (NSString *weekdaySymbol in calendar.weekdaySymbols) {
@@ -84,6 +93,75 @@
             NSInteger parsedWeekday = [calendar component:NSCalendarUnitWeekday fromDate:parsedDate];
             XCTAssertEqual(parsedWeekday, weekday, @"locale '%@' weekday '%@'", localeLanguageCode, weekdaySymbol);
             weekday++;
+        }
+    }
+}
+
+- (void)testDateCompletionSupportedLocales;
+{
+    NSArray *localizations = [bundle localizations];
+
+    for (NSString *localeLanguageCode in localizations) {
+        if ([localeLanguageCode isEqualToString:@"hr"]) // have a .strings file for Croatian, but no Date::Manip support
+            continue;
+
+        if (![self setUpForLocaleLanguageCode:localeLanguageCode])
+            continue;
+
+        NSDictionary *dateCompletions = [NSDictionary dictionaryWithContentsOfFile:[bundle pathForResource:@"DateCompletions" ofType:@"strings" inDirectory:nil forLocalization:localeLanguageCode]];
+
+        for (NSString *unlocalizedDateCompletion in dateCompletions) {
+            NSString *localizedDateCompletion = dateCompletions[unlocalizedDateCompletion];
+            NSDateComponents *components = [[NSDateComponents alloc] init];
+            NSDate *date = [NSDate date];
+            date = [calendar startOfDayForDate:date];
+
+            if ([unlocalizedDateCompletion isEqualToString:@"today"]) {
+                // today
+            } else if ([unlocalizedDateCompletion isEqualToString:@"tomorrow"]) {
+                components.day = 1;
+            } else if ([unlocalizedDateCompletion isEqualToString:@"in 2 days"]) {
+                components.day = 2;
+            } else if ([unlocalizedDateCompletion isEqualToString:@"next week"]) {
+                components.weekOfMonth = 1;
+            } else if ([unlocalizedDateCompletion isEqualToString:@"in 2 weeks"]) {
+                components.weekOfMonth = 2;
+            } else if ([unlocalizedDateCompletion isEqualToString:@"next month"]) {
+                components.month = 1;
+            } else if ([unlocalizedDateCompletion isEqualToString:@"in 2 months"]) {
+                components.month = 2;
+            } else if ([unlocalizedDateCompletion isEqualToString:@"in 1 year"]) {
+                components.year = 1;
+            } else if ([unlocalizedDateCompletion isEqualToString:@"next «day»"]) {
+                NSInteger weekday = 1;
+                for (NSString *weekdaySymbol in calendar.weekdaySymbols) {
+                    NSDate *nextWeekdayDate = [calendar dateBySettingUnit:NSCalendarUnitWeekday value:weekday ofDate:date options:NSCalendarMatchStrictly];
+                    weekday++;
+                    if ([date earlierDate:nextWeekdayDate] == nextWeekdayDate)
+                        nextWeekdayDate = [calendar dateByAddingUnit:NSCalendarUnitWeekOfMonth value:1 toDate:nextWeekdayDate options:NSCalendarMatchStrictly];
+                    NSString *localizedNextWeekday = [localizedDateCompletion stringByReplacingOccurrencesOfString:@"«day»" withString:weekdaySymbol];
+                    NSDate *parsedDate = parse_natural_language_date_on_queue(localizedNextWeekday);
+                    XCTAssertNotNil(parsedDate, @"locale '%@' next weekday '%@'", localeLanguageCode, localizedNextWeekday);
+                    if (parsedDate == nil)
+                        continue;
+                    parsedDate = [calendar startOfDayForDate:parsedDate];
+                    XCTAssertEqualObjects(parsedDate, nextWeekdayDate, @"locale '%@' next weekday '%@'", localeLanguageCode, localizedNextWeekday);
+                }
+                continue;
+            } else {
+                XCTFail(@"unexpected date completion '%@'", unlocalizedDateCompletion);
+                continue;
+            }
+
+            date = [calendar dateByAddingComponents:components toDate:date options:NSCalendarMatchStrictly];
+            [components release];
+
+            NSDate *parsedDate = parse_natural_language_date_on_queue(localizedDateCompletion);
+            XCTAssertNotNil(parsedDate, @"locale '%@' date completion '%@'", localeLanguageCode, localizedDateCompletion);
+            if (parsedDate == nil)
+                continue;
+            parsedDate = [calendar startOfDayForDate:parsedDate];
+            XCTAssertEqualObjects(parsedDate, date, @"locale '%@' date completion '%@'", localeLanguageCode, localizedDateCompletion);
         }
     }
 }
