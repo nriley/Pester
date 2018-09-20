2ebd7ac7767af05a5dd9af98b6aa7c229c434aa1
Nicholas Riley <com-github@sabi.net>
2018-09-20 15:31:58 -0400

    Remove support for pre-10.10.

--- Source/NJROperatingSystemVersion.m
+++ Source/NJROperatingSystemVersion.m
@@ -15,24 +15,10 @@ static NSInteger patchVersion;
 NSInteger NJROSXMinorVersion(void) {
     if (majorVersion == 0) {
         NSProcessInfo *processInfo = [NSProcessInfo processInfo];
-        if ([processInfo respondsToSelector: @selector(operatingSystemVersion)]) {
-            NSOperatingSystemVersion version = [processInfo operatingSystemVersion];
-            majorVersion = version.majorVersion;
-            minorVersion = version.minorVersion;
-            patchVersion = version.patchVersion;
-        } else {
-            // XXX remove when 10.10 required
-            SInt32 majorVersion32, minorVersion32, patchVersion32;
-            if (Gestalt(gestaltSystemVersionMajor, &majorVersion32) != noErr ||
-                Gestalt(gestaltSystemVersionMinor, &minorVersion32) != noErr ||
-                Gestalt(gestaltSystemVersionBugFix, &patchVersion32) != noErr) {
-                majorVersion = minorVersion = patchVersion = -1;
-            } else {
-                majorVersion = majorVersion32;
-                minorVersion = minorVersion32;
-                patchVersion = patchVersion32;
-            }
-        }
+        NSOperatingSystemVersion version = [processInfo operatingSystemVersion];
+        majorVersion = version.majorVersion;
+        minorVersion = version.minorVersion;
+        patchVersion = version.patchVersion;
     }
 
     if (majorVersion != 10)
